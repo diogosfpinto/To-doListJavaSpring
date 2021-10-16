a76df11a04b84dd0f322622dc25e0ea916fb48b7
diogosfpinto <diogo.sfpinto@gmail.com>
2021-10-16 16:31:34 -0400

    implementado create e read da lista de tarefas

--- src/main/java/com/kamilacode/todolistyoutube/repository/TaskRepository.java
+++ src/main/java/com/kamilacode/todolistyoutube/repository/TaskRepository.java
@@ -1,5 +1,14 @@
 package com.kamilacode.todolistyoutube.repository;
 
+import com.kamilacode.todolistyoutube.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TaskRepository {
+/*
+* Interface para Repository
+* */
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+
 }
